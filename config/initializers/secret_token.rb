ad0a9e315a9a56842f1824f184452d9b70ddb0d7
Tim Harvey <tim@literacy5.com>
2012-01-16 12:21:52 -0500

    Initial commit: Empty Rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PomoCount::Application.config.secret_token = 'e636a7ec8f1337cf60aab91f037094e71a2b30c36e82cd461481e7f01928fc2640af0c3807aa2970523924cf83370820b5963d12c26529371889056b26a428cc'
